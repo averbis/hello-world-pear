33857a26d8808aa66b27dee3d56d37d27e32e9d5
Christian Gaege <christian.gaege@averbis.com>
2018-11-23 21:38:12 +0100

    Initial commit of maven project structure

---
+++ src/main/java/de/averbis/tutorials/HelloWorldAnnotator.java
@@ -0,0 +1,17 @@
+/**
+ * Hello World Annotator implementation
+ */
+package de.averbis.tutorials;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+public class HelloWorldAnnotator extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+		
+		/* Not implemented yet */
+	}
+}
