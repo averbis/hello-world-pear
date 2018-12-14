f1e8e833fbcc6a8c8344abc16cc1b8d9c898cc62
Christian Gaege <christian.gaege@averbis.com>
2018-12-14 10:12:04 +0100

    Added greeting parameter to HelloWorldAnnotator

--- src/main/java/de/averbis/tutorials/HelloWorldAnnotator.java
+++ src/main/java/de/averbis/tutorials/HelloWorldAnnotator.java
@@ -1,13 +1,35 @@
 package de.averbis.tutorials;
 
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import de.averbis.tutorials.typesystem.HelloWorldAnnotation;
 
 public class HelloWorldAnnotator extends JCasAnnotator_ImplBase {
 
+	/*
+	 * the greeting message
+	 */
+	private String greeting;
+	
+	/*
+	 * parameters
+	 */
+	public static final String GREETING_PARAMETER_NAME = "greeting";
+	
+	
+	@Override
+	public void initialize(UimaContext uimaContext) throws ResourceInitializationException {
+		
+		super.initialize(uimaContext);
+		
+		this.greeting = (String) uimaContext.getConfigParameterValue(GREETING_PARAMETER_NAME);
+	}
+	
+	
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		
@@ -20,7 +42,9 @@ public class HelloWorldAnnotator extends JCasAnnotator_ImplBase {
 		HelloWorldAnnotation annotation = new HelloWorldAnnotation(jcas);
 		annotation.setBegin(0);
 		annotation.setEnd(documentText.length());
-		annotation.setGreeting("Greetings from the HelloWorldAnnotator");
+		annotation.setGreeting(this.greeting);
 		annotation.addToIndexes();
+		
+		
 	}
 }
