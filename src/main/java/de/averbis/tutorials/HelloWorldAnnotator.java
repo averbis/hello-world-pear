a23cec3bc0f415b731acf5d2b2d017aad58958f4
Richard Eckart de Castilho <richard.eckart@averbis.com>
2021-03-25 12:47:27 +0100

    Issue #1: Upgrade to UIMAv3
    - Switch to UIMAv3
    - Clean up POM
    - Added descriptive comments in the POM
    - Use the uimaFIT "enhance" goal
    - Clean up and condense example and test code

--- src/main/java/de/averbis/tutorials/HelloWorldAnnotator.java
+++ src/main/java/de/averbis/tutorials/HelloWorldAnnotator.java
@@ -1,19 +1,18 @@
 /*
- * Copyright 2018 Averbis GmbH
- * 
+ * Copyright 2021 Averbis GmbH
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.averbis.tutorials;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -24,29 +23,22 @@ import org.apache.uima.jcas.JCas;
 import de.averbis.tutorials.typesystem.HelloWorldAnnotation;
 
 public class HelloWorldAnnotator extends JCasAnnotator_ImplBase {
-	
+
+	/**
+	 * The greeting message.
+	 */
 	public static final String PARAM_GREETING = "greeting";
-	@ConfigurationParameter(
-			name = PARAM_GREETING,
-			mandatory = true,
-			defaultValue = "Greetings from the HelloWorldAnnotator",
-			description = "The greeting message")
+	@ConfigurationParameter(name = PARAM_GREETING, defaultValue = "Hi from the HelloWorldAnnotator")
 	private String greeting;
-	
+
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		
-		/*
-		 * Adds a new HelloWorldAnnotation with a greeting 
-		 * that spans the entire document text
-		 */
-		String documentText = jcas.getDocumentText();
-		
-		HelloWorldAnnotation annotation = new HelloWorldAnnotation(jcas);
-		annotation.setBegin(0);
-		annotation.setEnd(documentText.length());
-		annotation.setGreeting(this.greeting);
+
+		// Adds a new HelloWorldAnnotation with a greeting that spans the entire document text
+		HelloWorldAnnotation annotation = new HelloWorldAnnotation(jcas, 0,
+				jcas.getDocumentText().length());
+		annotation.setGreeting(greeting);
 		annotation.addToIndexes();
 	}
 }
